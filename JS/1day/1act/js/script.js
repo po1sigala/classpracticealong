9da40fe275b97d7e3dd48fb87ed445a65aa79853
Your Name <alexsigala87@gmail.com>
2021-04-05 20:27:49 -0700

    added folders for all acts started assignment one dynamically creating elements with JS. page ads one div with text

---
+++ JS/1day/1act/js/script.js
@@ -0,0 +1,14 @@
+// Create your HTML Page via DOM Methods here!
+console.log("hello");
+var body = document.body;
+
+let newDiv = document.createElement("div");
+newDiv.textContent = "HELOOOOOOOOOO";
+
+// TODO: Add a centered h1
+body.appendChild(newDiv);
+// TODO: Add a centered h2
+
+// TODO: Add a centered image with a centered caption under it
+
+// TODO: Add a list of your favorite foods (or other favorites)
